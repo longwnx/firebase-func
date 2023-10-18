541683de541160e41a8832b272f93cbecc5e4230
Long Nguyen Xuan <nxl232@gmail.com>
2023-10-18 17:42:57 +0700

    feat: Change dev nodemon

--- functions/src/server.ts
+++ functions/src/server.ts
@@ -9,11 +9,13 @@ import productRoutes from "./routes/productRoutes";
 
 const app = express();
 // **** Setup **** //
-
+const port = 3000;
 // Basic middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 // Sử dụng route mới
 app.use("/app-mobile/v1", appRoute);
 app.use("/catalog/v1", productRoutes);
