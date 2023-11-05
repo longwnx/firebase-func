7880fc581ccf93e7706b3fc5d603ce9933e8385a
Nguyen Xuan Long <long.nguyenxuan@jmango360.com>
2023-11-05 14:48:30 +0700

    fix: Delete cart item

--- functions/src/routes/orderRoutes.ts
+++ functions/src/routes/orderRoutes.ts
@@ -21,7 +21,7 @@ router.post("/create", handleCreateOrderRequest);
 router.get("/carts", handleGetCartRequest);
 router.post("/carts/create", handleAddItemCartRequest);
 router.put("/carts/:cartId/items", handleUpdateCartRequest);
-router.delete("/carts/:cartId/items/:productId", handleDeleteCartItemRequest);
+router.delete("/carts/:cartId/items/:itemId", handleDeleteCartItemRequest);
 router.post("/carts/update/customer", handleCustomerCartRequest);
 router.put("/carts/:cartId/items/qty", handleUpdateQuantityRequest);
 export default router;
