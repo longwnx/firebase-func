35d144e83a41f9a0413bd506ead5014c05fb09cc
Long Nguyen Xuan <nxl232@gmail.com>
2023-12-23 20:57:02 +0700

    feat: Config lamda aws

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,16 @@
-import {onRequest} from "firebase-functions/v2/https";
-import express from "express";
+// import {onRequest} from "firebase-functions/v2/https";
 import {app} from "./server";
+import awsServerlessExpress from "aws-serverless-express";
+import {APIGatewayProxyEvent, Context} from "aws-lambda";
 
-const expressApp = express();
+// const expressApp = express();
 
-expressApp.use("/", app);
+// expressApp.use("/", app);
 
-export const helloWorld = onRequest(expressApp);
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = async (event: APIGatewayProxyEvent, context: Context) => {
+  // Use aws-serverless-express to proxy the event to your Express app
+  return awsServerlessExpress.proxy(server, event, context);
+};
+// export default onRequest(expressApp);
