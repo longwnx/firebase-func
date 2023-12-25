84b4ccca7e3bfe5bc2e6c0cfb3d00fffe65019e3
Long Nguyen Xuan <nxl232@gmail.com>
2023-12-25 14:59:45 +0700

    fix: Fix connect docker

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,35 +1,15 @@
 // import {onRequest} from "firebase-functions/v2/https";
 import {app} from "./server";
 import express from "express";
-import serverless from "serverless-http";
+import awsServerlessExpress from "aws-serverless-express";
+import {APIGatewayProxyEvent, Context} from "aws-lambda";
 
 const expressApp = express();
 
 expressApp.use("/", app);
-// app.use(
-//   (req: express.Request, res: express.Response, next: express.NextFunction) => {
-//     res.status(404).send();
-//   },
-// );
-//
-// app.use(
-//   (
-//     err: any,
-//     req: express.Request,
-//     res: express.Response,
-//     next: express.NextFunction,
-//   ) => {
-//     res.status(err.status || 500).send();
-//   },
-// );
-// const server = awsServerlessExpress.createServer(app);
-//
-// exports.handler = async (event: APIGatewayProxyEvent, context: Context) => {
-//   // Use aws-serverless-express to proxy the event to your Express app
-//   return awsServerlessExpress.proxy(server, event, context);
-// };
-// // export default onRequest(expressApp);
 
-// export const handler = serverless(app);
-
-module.exports.handler = serverless(expressApp);
+const server = awsServerlessExpress.createServer(app);
+//
+exports.handler = async (event: APIGatewayProxyEvent, context: Context) => {
+  return awsServerlessExpress.proxy(server, event, context);
+};
