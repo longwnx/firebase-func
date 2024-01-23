6e2c575d82b36d99cbd555094227699f657317e0
Long Nguyen Xuan <nxl232@gmail.com>
2024-01-23 15:08:44 +0700

    feat: Update firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
 import {onRequest} from "firebase-functions/v2/https";
 import express from "express";
 import {app} from "./server";
+import {setGlobalOptions} from "../node_modules/firebase-functions/lib/v2";
 
 const expressApp = express();
 
 expressApp.use("/", app);
 
+// Set the maximum instances to 10 for all functions
+setGlobalOptions({maxInstances: 10});
 export const ecomServices = onRequest(expressApp);
